96ebe6cd6922a7fe34b5e4771af77280cce3854f
Azizbek Gaybullaev <azizgaybullaev@mail.ru>
2020-08-16 11:27:23 +0500

    modfied

---
+++ Easy Problems/first_bad_version.py
@@ -0,0 +1,11 @@
+def firstBadVersion(n):
+    left = 1
+    right = n
+
+    while left < n:
+        mid = left + (right - left) // 2
+        if isBadVersion(mid):
+            right = mid
+        else:
+            left = mid + 1
+        return left
