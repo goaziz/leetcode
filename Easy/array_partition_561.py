f07050892cda23e316a00beb7d2cf1f944df8f8a
goaziz <goazizbek@gmail.com>
2023-12-21 12:07:51 +0500

    561. Array Partition

---
+++ Easy/array_partition_561.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+
+        total = 0
+        for i in range(0, len(nums), 2):
+            total += nums[i]
+
+        return total
+
+
+obj = Solution()
+print(obj.arrayPairSum(nums=[1, 4, 3, 2]))
+print(obj.arrayPairSum(nums=[6, 2, 6, 5, 1, 2]))
