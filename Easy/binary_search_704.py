19506b8c275c6b01b52c31183a5d442f1b504999
goaziz <goazizbek@gmail.com>
2024-01-05 16:36:34 +0500

    704. Binary Search

---
+++ Easy/binary_search_704.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return -1
+
+
+obj = Solution()
+nums = [-1, 0, 3, 5, 9, 12]
+target = 3
+print(obj.search(nums, target))
