b3f6bec2020596d898efd4b04d9c0bc9935efef0
goaziz <goazizbek@gmail.com>
2024-01-12 16:31:57 +0500

    796. Rotate String

--- Easy/rotate_string_796.py
+++ Easy/rotate_string_796.py
@@ -1,13 +1,28 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
-        if len(s) != len(goal):
+        return goal in s + s and len(s) == len(goal)
+
+    def rotateString2(self, s: str, goal: str) -> bool:
+        if s != goal:
             return False
 
-        temp = s + s
-        return goal in temp
+        s, goal = [*s], [*goal]
+
+        # unpacking the strings (creates list)
+        # ex: 'hello' -> ['h','e','l','l','o']
+
+        for x in range(len(s)):
+            a = s[0]
+            s.pop(0)
+            s.append(a)
+            
+            if s == goal:
+                return True
+
+        return False
 
 
 obj = Solution()
 s = 'abcde'
 goal = 'abced'
-print(obj.rotateString(s, goal))
+print(obj.rotateString2(s, goal))
