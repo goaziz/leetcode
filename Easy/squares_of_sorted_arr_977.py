b4e5149a9ce0f72a3e704fa4f442822513e7aa35
goaziz <goazizbek@gmail.com>
2024-02-03 18:29:59 +0500

    977. Squares of a Sorted Array

--- Easy/squares_of_sorted_arr_977.py
+++ Easy/squares_of_sorted_arr_977.py
@@ -5,5 +5,25 @@ class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
         nums = [i * i for i in nums]
         nums.sort()
-        
+
         return nums
+
+    def sortedSquares2(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        result = [0] * n
+        left = 0
+        right = n - 1
+        for i in range(n - 1, -1, -1):
+            if abs(nums[left]) < abs(nums[right]):
+                square = nums[right]
+                right -= 1
+            else:
+                square = nums[left]
+                left += 1
+            result[i] = square * square
+        return result
+
+
+obj = Solution()
+nums = [-3, -2, 0, 6, 9, 12]
+print(obj.sortedSquares2(nums))
