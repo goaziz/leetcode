962f1dc76214304ad1c76b128485fccfd4774547
goaziz <goazizbek@gmail.com>
2024-03-07 10:51:51 +0500

    83. Remove Duplicates from Sorted List

--- Easy/remove_duplicates_from_llist_83.py
+++ Easy/remove_duplicates_from_llist_83.py
@@ -12,10 +12,19 @@ class ListNode:
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         current = head
-        while current:
+        while current is not None and current.next is not None:
             if current.val == current.next.val:
                 current.next = current.next.next
+            else:
+                current = current.next
 
-            current = current.next
-        
         return head
+
+
+head = ListNode(1)
+head.next = ListNode(2)
+head.next.next = ListNode(3)
+head.next.next.next = ListNode(3)
+head.next.next.next.next = ListNode(4)
+obj = Solution()
+print(obj.deleteDuplicates(head))
