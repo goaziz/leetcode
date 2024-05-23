8a45dd82cb760a9e9018dd4a24ab9defdcef021e
goaziz <azizgaybullaev@mail.ru>
2024-05-23 14:37:16 +0500

    1422. Maximum Score After Splitting a String (Updated)

--- Easy/max_score_after_split_str_1422.py
+++ Easy/max_score_after_split_str_1422.py
@@ -1,15 +1,33 @@
 class Solution:
     def maxScore(self, s: str) -> int:
-        sums = []
+        max_score = 0
 
         for i in range(1, len(s)):
             zeroes = s[:i].count('0')
             ones = s[i:].count('1')
-            sums.append(ones + zeroes)
+            max_score = max(max_score, zeroes + ones)
 
-        return max(sums)
+        return max_score
+
+    def maxScore2(self, s: str) -> int:
+        ones = 0
+        zeros = 0
+        best = float('-inf')
+
+        for i in range(len(s) - 1):
+            if s[i] == "1":
+                ones += 1
+            else:
+                zeros += 1
+
+            best = max(best, zeros - ones)
+
+        if s[-1] == "1":
+            ones += 1
+
+        return best + ones
 
 
 obj = Solution()
-s = "00"
+s = "011101"
 print(obj.maxScore2(s))
