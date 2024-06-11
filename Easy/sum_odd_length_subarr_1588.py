a6be168144a8949b7927d5befa96eb646377b8c4
goaziz <azizgaybullaev@mail.ru>
2024-06-11 08:28:24 +0500

    1588. Sum of All Odd Length Subarrays

--- Easy/sum_odd_length_subarr_1588.py
+++ Easy/sum_odd_length_subarr_1588.py
@@ -13,7 +13,31 @@ class Solution:
 
         return s
 
+    def sumOddLengthSubarrays2(self, arr: List[int]) -> int:
+        n = len(arr)
+        total = 0
+
+        for i in range(n):
+            end = i + 1
+            start = n - i
+            total_subarrays = start * end
+            odd_count = (total_subarrays + 1) // 2
+            total += arr[i] * odd_count
+
+        return total
+
+    def sumOddLengthSubarrays3(self, arr: List[int]) -> int:
+        n = len(arr)
+        answer = 0
+
+        for left in range(n):
+            current_sum = 0
+            for right in range(left, n):
+                current_sum += arr[right]
+                answer += current_sum if (right - left + 1) % 2 == 1 else 0
+        return answer
+
 
 obj = Solution()
-arr = [10, 11, 12]
-print(obj.sumOddLengthSubarrays(arr))
+arr = [1, 4, 2, 5, 3]
+print(obj.sumOddLengthSubarrays3(arr))
