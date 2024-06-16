4c9c28221613bab14ec785bb13b85ae3a13d1056
goaziz <azizgaybullaev@mail.ru>
2024-06-16 17:46:19 +0500

    1640. Check Array Formation Through Concatenation

---
+++ Easy/can_form_arr_1640.py
@@ -0,0 +1,12 @@
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def canFormArray(self, arr: List[int], pieces: List[List[int]]) -> bool:
+        pass
+
+obj = Solution()
+arr = [15, 88]
+pieces = [[88], [15]]
+print(obj.canFormArray(arr, pieces))
