5f41940d5fe861183dfb0316c9d6c58f4f365f37
goaziz <azizgaybullaev@mail.ru>
2024-06-29 15:55:00 +0500

    1736. Latest Time by Replacing Hidden Digits

---
+++ Easy/latest_valid_time_1736.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maximumTime(self, time: str) -> str:
+        time = list(time)
+
+        if time[0] == '?':
+            if time[1] == '?' or time[1] in '0123':
+                time[0] = '2'
+            else:
+                time[0] = '1'
+
+        if time[1] == '?':
+            if time[0] == '2':
+                time[1] = '3'
+            else:
+                time[1] = '9'
+
+        if time[3] == '?':
+            time[3] = '5'
+
+        if time[4] == '?':
+            time[4] = '9'
+
+        return ''.join(time)
+
+
+obj = Solution()
+time = "1?:22"
+print(obj.maximumTime(time))
