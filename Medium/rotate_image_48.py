d88ed1e8a75ba1a872d4454eecbef792711e8d4f
goaziz <azizgaybullaev@mail.ru>
2024-07-05 14:26:07 +0500

    48. Rotate Image

---
+++ Medium/rotate_image_48.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        for row in matrix:
+            row.reverse()
+
+
+obj = Solution()
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print(obj.rotate(matrix))
