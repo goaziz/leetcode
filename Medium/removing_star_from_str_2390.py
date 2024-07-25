7fb77dbeed4cbd3b63eabc602510e3e8eb21346b
goaziz <azizgaybullaev@mail.ru>
2024-07-25 17:55:29 +0500

    2390. Removing Stars From a String

---
+++ Medium/removing_star_from_str_2390.py
@@ -0,0 +1,16 @@
+class Solution:
+    def removeStars(self, s: str) -> str:
+        stack = []
+
+        for char in s:
+            if char == '*' and stack:
+                stack.pop()
+            else:
+                stack.append(char)
+
+        return ''.join(stack)
+
+
+obj = Solution()
+s = "erase*****"
+print(obj.removeStars(s))
