21410f42b4b3e97fc6f0016d4899f91489d8e8ce
goaziz <azizgaybullaev@mail.ru>
2024-08-02 17:10:23 +0500

    901. Online Stock Span

---
+++ Medium/online_stock_span_901.py
@@ -0,0 +1,24 @@
+class StockSpanner:
+
+    def __init__(self):
+        self.stack = []
+
+    def next(self, price: int) -> int:
+        span = 1
+
+        while self.stack and self.stack[-1][0] <= price:
+            span += self.stack.pop()[1]
+
+        self.stack.append((price, span))
+
+        return span
+
+
+obj = StockSpanner()
+print(obj.next(100))
+print(obj.next(80))
+print(obj.next(60))
+print(obj.next(70))
+print(obj.next(60))
+print(obj.next(75))
+print(obj.next(85))
