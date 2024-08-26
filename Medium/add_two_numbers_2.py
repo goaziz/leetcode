d08e232dd69bb8a288858898af80a2b3b58b41ca
goaziz <azizgaybullaev@mail.ru>
2024-08-26 18:14:58 +0500

    2. Add Two Numbers

--- Medium/add_two_numbers_2.py
+++ Medium/add_two_numbers_2.py
@@ -23,18 +23,6 @@ class Solution:
 
         return head
 
-    def reverse(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev = None
-        curr = head
-
-        while curr:
-            next_node = curr.next
-            curr.next = prev
-            prev = curr
-            curr = next_node
-
-        return prev
-
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         carry = 0
         dummy = ListNode(0)
