6928219e292de879c09e7aef789f7593137cb6e1
goaziz <azizgaybullaev@mail.ru>
2024-08-28 15:36:36 +0500

    1209. Remove All Adjacent Duplicates in String II

--- Medium/remove_all_adjacent_dup_1209.py
+++ Medium/remove_all_adjacent_dup_1209.py
@@ -5,7 +5,7 @@ class Solution:
         for char in s:
             if stack and stack[-1][0] == char:
                 stack[-1][1] += 1
-                while stack and stack[-1][1] == k:
+                if stack[-1][1] == k:
                     stack.pop()
             else:
                 stack.append([char, 1])
