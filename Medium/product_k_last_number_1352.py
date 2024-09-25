7b1118c15ed0cee08cccf4284fd350621a76bbb1
goaziz <azizgaybullaev@mail.ru>
2024-09-25 10:47:42 +0500

    1352. Product of the Last K Numbers

---
+++ Medium/product_k_last_number_1352.py
@@ -0,0 +1,44 @@
+import math
+from collections import deque
+
+
+class ProductOfNumbers:
+
+    def __init__(self):
+        self.queue = deque()
+
+    def add(self, num: int) -> None:
+        self.queue.appendleft(num)
+
+    def getProduct(self, k: int) -> int:
+        l = list(self.queue)
+        return math.prod(l[:k])
+
+
+class ProductOfNumbers2:
+
+    def __init__(self):
+        self.nums = [1]
+
+    def add(self, num: int) -> None:
+        if num == 0:
+            self.nums = [1]
+        else:
+            self.nums.append(self.nums[-1] * num)
+
+    def getProduct(self, k: int) -> int:
+        print(self.nums)
+        if k >= len(self.nums):
+            return 0
+
+        return self.nums[-1] // self.nums[-1-k]
+
+
+obj = ProductOfNumbers2()
+print(obj.add(1))
+print(obj.add(2))
+print(obj.add(3))
+print(obj.add(4))
+print(obj.add(5))
+print(obj.add(6))
+print(obj.getProduct(2))
