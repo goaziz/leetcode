16e98470ba2fd159d9257e4c5f8eee0e67b2e08d
goaziz <azizgaybullaev@mail.ru>
2024-11-22 10:47:40 +0500

    347. Top K Frequent Elements

--- Medium/top_k_freq_element_347.py
+++ Medium/top_k_freq_element_347.py
@@ -6,14 +6,26 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         freq = Counter(nums)
         s = sorted(freq.items(), key=lambda x: x[1], reverse=True)
-        
+
         result = []
         for key, v in s:
             if len(result) == k:
                 return result
             result.append(key)
 
+    def topKFrequent2(self, nums: List[int], k: int) -> List[int]:
+        freq = Counter(nums)
+        buckets = [[] for _ in range(len(nums) + 1)]
+
+        for key, val in freq.items():
+            buckets[val].append(key)
+
+        buckets.reverse()
+        result = [item for val in buckets for item in val]
+
+        return result[:k]
+
 
 obj = Solution()
 nums = [2, 2, 2, 3, 4, 3, 4, 5, 3, 3]
-print(obj.topKFrequent(nums, 2))
+print(obj.topKFrequent2(nums, 2))
