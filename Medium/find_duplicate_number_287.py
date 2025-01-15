622919d287d69fc5e8aaddf2b8fa05d6cb367ce5
goaziz <azizgaybullaev@mail.ru>
2025-01-15 16:59:02 +0500

    287. Find the Duplicate Number

---
+++ Medium/find_duplicate_number_287.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow = nums[0]
+        fast = nums[0]
+
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+
+            if slow == fast:
+                break
+
+        slow = nums[0]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+
+        return fast
+
+
+obj = Solution()
+nums = [1, 3, 4, 2, 2]
+print(obj.findDuplicate(nums))
