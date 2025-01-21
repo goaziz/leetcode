1435ac2dfe88b532846d139d2163d2a653142ac0
goaziz <azizgaybullaev@mail.ru>
2025-01-21 12:17:59 +0500

    540. Single Element in a Sorted Array

---
+++ Medium/single_element_sorted_arr_540.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left < right:
+            mid = (left + right) // 2
+
+            if mid % 2 == 1:
+                mid -= 1
+
+            if nums[mid] == nums[mid + 1]:
+                left = mid + 2
+            else:
+                right = mid
+
+        return nums[left]
+
+
+obj = Solution()
+nums = [3, 3, 7, 7, 10, 11, 11]
+print(obj.singleNonDuplicate(nums))
