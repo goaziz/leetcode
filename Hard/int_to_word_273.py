056cc5b202a9326d3d4678fbfc3a188558665771
goaziz <azizgaybullaev@mail.ru>
2025-06-17 14:17:14 +0500

    Integer to words 273

--- Hard/int_to_word_273.py
+++ Hard/int_to_word_273.py
@@ -8,14 +8,14 @@ class Solution:
             4: "Four", 5: "Five", 6: "Six", 7: "Seven",
             8: "Eight", 9: "Nine", 10: "Ten", 11: "Eleven",
             12: "Twelve", 13: "Thirteen", 14: "Fourteen", 15: "Fifteen",
-            16: "Sixteen", 17: "Seventeen", 18: "Eighteen", 19: "Nineteen",
-            100: "Hundred", 1000: "Thousand", 1000000: "Million", 1000000000: "Billion"
+            16: "Sixteen", 17: "Seventeen", 18: "Eighteen", 19: "Nineteen"
         }
         groups = ["", "Thousand", "Million", "Billion"]
         result = ""
         group = 0
-        
-        multiple_of_ten = ['', '', 'Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety']
+
+        multiple_of_ten = ['', '', 'Twenty', 'Thirty', 'Forty',
+                           'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety']
 
         while num > 0:
             if num % 1000 != 0:
